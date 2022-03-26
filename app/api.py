ad05e9629320fbc852d1fe6803679aaa9a08e16f
gleb <gleb.omarov@gmail.com>
2022-03-27 00:31:58 +0300

    hackathon

--- app/api.py
+++ app/api.py
@@ -1,32 +1,54 @@
 """ ---------------------NEWS API------------------------------- """
 import re
-from newsapi import NewsApiClient
-
-newsapi = NewsApiClient(api_key='cb21ebc4040344cfa657a8490189aa29')
-# all data dictionary
-data_dict = {'breaking_news_long_string':'', 'articles': []}
-
-
-def get_news(word):
-    top_headlines = newsapi.get_top_headlines(language='en', sources='bbc-news')
-    data_dict['breaking_news_long_string'] = '\t\tBREAKING NEWS!\t'.join(
-        source['title'] for source in top_headlines['articles'])
-    if word == '':
-        for article in top_headlines['articles']:
-            data_dict['articles'].append({'title': article['title'],
-                                          'description': article['description'],
-                                          'content': re.sub(r'\[.*\]', '', article['content']),
-                                          'urlToImage': article['urlToImage'],
-                                          'author': article['author'],
-                                          'url': article['url']})
-
-
-""" ------------------------WEATHER API---------------------------- """                                         
-import json
 import requests
-my_key = 'c0779d2b68b69ef6b733d5629c17506f'
-l = 'londo'
-uResponse = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={l}&appid=c0779d2b68b69ef6b733d5629c17506f")
 
-Jresponse = uResponse.text
-data = json.loads(Jresponse)
+# all data dictionary
+data_dict = {'breaking_news_long_string': '', 'articles': []}
+
+
+def get_news(word=None):
+    data_dict['breaking_news_long_string'] = get_headlines()
+    if word == '' or word is None:
+        url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=1090ddedde734dbd979190fd2b5f0745"
+    else:
+        url = f"https://newsapi.org/v2/everything?q={word}&apiKey=1090ddedde734dbd979190fd2b5f0745"
+
+    response = requests.get(url).json()
+    if response["totalResults"] <= 0:
+        return get_news()
+    pattern = r'\[.*\]'
+    data_dict['articles'] = []
+    for article in response['articles']:
+        data_dict['articles'].append({'title': article['title'],
+                                      'description': article['description'],
+                                      'content': re.sub(pattern, ' ', str(article['content'])),
+                                      'urlToImage': article['urlToImage'],
+                                      'author': article['author'],
+                                      'url': article['url']})
+    return data_dict
+
+
+def get_headlines():
+    url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=1090ddedde734dbd979190fd2b5f0745'
+    top_headlines = requests.get(url).json()
+    headline = ""
+    for index, source in enumerate(top_headlines['articles']):
+        headline += '\t\tBREAKING NEWS!\t' + source['title']
+        if index >= 6:
+            break
+    return headline
+
+#
+#
+# """ ------------------------WEATHER API---------------------------- """
+# import json
+# import requests
+# my_key = 'c0779d2b68b69ef6b733d5629c17506f'
+# l = 'londo'
+# uResponse = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={l}&appid=c0779d2b68b69ef6b733d5629c17506f")
+#
+# Jresponse = uResponse.text
+# data = json.loads(Jresponse)
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,21 @@
 from flask import flash, redirect, render_template, request, url_for
 from app import app, db
-from app.api import data_dict, get_news
+from app.api import get_news
 from app.forms import RegistrationForm
 from app.models import User_data
 
 
 @app.route('/', methods=['GET', 'POST'])
-def refresh_all(word=''):
-    get_news(word)
+def refresh_all():
+    word = request.form.get("search_word") if request.method == "POST" else ""
+    data_dict = get_news(word)
     form = RegistrationForm()
-    if form.validate_on_submit():
-        return redirect(url_for('refresh_all'))
     return render_template('index.html', data_dict=data_dict, form=form)
+
+#
+# @app.route('/specify/', methods=['GET', 'POST'])
+# def specify():
+#     data_dict = get_news(request.form.get("search_word"))
+#     print("data_dict, clinton=", data_dict["articles"][0])
+#     form = RegistrationForm()
+#     return render_template('index.html', data_dict=data_dict, form=form)
